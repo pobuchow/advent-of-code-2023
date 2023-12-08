8bd397865efa9076b7b5c42fc14575e0c504c1bb
pobuchow <piotr.obuchowski.dev@gmail.com>
2023-12-08 22:55:41 +0100

    Day05 part 1

---
+++ src/Day05.kt
@@ -0,0 +1,43 @@
+fun main() {
+    fun part1(input: String): Long {
+        val inputGroups = input.split("\n\n")
+        val dictionary = inputGroups.drop(1).map {
+            val splitKeyValues = it.split(":")
+            Pair(splitKeyValues[0].split(" map")[0], splitKeyValues[1].split("\n").filter{ c -> c.isNotBlank()}.map { c -> c.trim() })
+        }.groupBy { it.first }.mapValues { it.value[0].second.map { l ->
+            val (target, source, length) = l.split(" ").map { c -> c.toLong() }
+            Triple(target, source, length)
+        } }
+
+        val seeds = inputGroups[0].split(": ")[1].split(" ").map { it.toLong() }
+
+        fun mapWithDictionary(
+            key: String,
+            element: Long
+        ) = dictionary[key]!!.find { m -> element in (m.second..m.third.plus(m.second).minus(1)) }?.let { m ->
+            element.plus(m.first - m.second)
+        } ?: element
+
+        return seeds.asSequence().map {
+            mapWithDictionary("seed-to-soil", it)
+        }.map {
+            mapWithDictionary("soil-to-fertilizer", it)
+        }.map {
+            mapWithDictionary("fertilizer-to-water", it)
+        }.map {
+            mapWithDictionary("water-to-light", it)
+        }.map {
+            mapWithDictionary("light-to-temperature", it)
+        }.map {
+            mapWithDictionary("temperature-to-humidity", it)
+        }.minOfOrNull {
+            mapWithDictionary("humidity-to-location", it)
+        }?:0
+    }
+
+    val testInput1 = readInputText("Day05_test")
+    check(part1(testInput1) == 35L)
+
+    val input = readInputText("Day05")
+    part1(input).println()
+}
