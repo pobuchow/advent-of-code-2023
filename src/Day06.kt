48895f64be9eef11480cc0985cff0216aeec97ca
pobuchow <piotr.obuchowski.dev@gmail.com>
2023-12-18 09:57:03 +0100

    Day 06 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,14 +2,22 @@ fun main() {
     fun part1(input: List<String>): Int {
         val timeSeries = input[0].split(":")[1].split(" ").filter { it.isNotBlank() }.map { it.toLong() }
         val distanceSeries = input[1].split(":")[1].split(" ").filter { it.isNotBlank() }.map { it.toLong() }
-        return calc(timeSeries, distanceSeries)
+        return calc(timeSeries, distanceSeries).reduce(Int::times)
+    }
+
+    fun part2(input: List<String>): Int {
+        val timeSeries = input[0].split(":")[1].split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+        val distanceSeries = input[1].split(":")[1].split(" ").filter { it.isNotBlank() }.joinToString("").toLong()
+        return calc(arrayListOf(timeSeries), arrayListOf(distanceSeries)).first()
     }
 
     val testInput1 = readInput("Day06_test")
     check(part1(testInput1) == 288)
+    check(part2(testInput1) == 71503)
 
     val input = readInput("Day06")
     part1(input).println()
+    part2(input).println()
 }
 
 fun calc(
@@ -19,4 +27,4 @@ fun calc(
     (0..time).toList().filter { t -> t * (time - t) > distance }
 }.map {
     it.size
-}.reduce(Int::times)
+}
