396538cdd8f0cff02b2a8ef0e88420c70d2ccd65
pobuchow <piotr.obuchowski.dev@gmail.com>
2023-12-29 14:20:45 +0100

    Day 09 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,17 +1,29 @@
 fun main() {
     fun part1(input: List<String>): Long = input.sumOf { l ->
-        getDiff(l.split(" ").map { it.toLong() })
+        futureExtrapolation(l.split(" ").map { it.toLong() })
+    }
+
+    fun part2(input: List<String>): Long = input.sumOf { l ->
+        pastExtrapolation(l.split(" ").map { it.toLong() })
     }
 
     val testInput = readInput("Day09_test")
     check(part1(testInput) == 114L)
+    check(part2(testInput) == 2L)
 
     val input = readInput("Day09")
     part1(input).println()
+    part2(input).println()
 }
 
-fun getDiff(input: List<Long>): Long = if(input.all { it == input[0] }){
+fun futureExtrapolation(input: List<Long>): Long = if(input.all { it == input[0] }){
     input[0]
 } else {
-    input.last() + getDiff(input.zipWithNext { first, second -> second - first })
+    input.last() + futureExtrapolation(input.zipWithNext { first, second -> second - first })
+}
+
+fun pastExtrapolation(input: List<Long>): Long = if(input.all { it == input[0] }){
+    input[0]
+} else {
+    input.first() - pastExtrapolation(input.zipWithNext { first, second -> second - first })
 }
